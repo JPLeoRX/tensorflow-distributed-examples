d8780389fb286bbe7d4236a133d8bc6217081520
leo <leo.ertuna@gmail.com>
2019-09-24 14:50:35 +0300

    Run only on remote

--- Distributed/mnist_sample/distributed_mnist_run_0.py
+++ Distributed/mnist_sample/distributed_mnist_run_0.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import json
 import os
-from Distributed.mnist_sample.distributed_mnist import main
+from distributed_mnist import main
 
 # Create cluster
 # This variable must be set on each worker with changing index
 os.environ["TF_CONFIG"] = json.dumps({
     'cluster': {
-        'worker': ["localhost:2222", "10.111.54.32:2222"]
+        'worker': ["10.111.54.31:2222", "10.111.54.32:2222"]
     },
     'task': {'type': 'worker', 'index': 0}
 })

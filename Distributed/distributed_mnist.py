b6f56551cd61367e0650aeeccdcce33b1e87c48f
leo <leo.ertuna@gmail.com>
2019-09-24 11:45:40 +0300

    Removed unused imports

--- Distributed/distributed_mnist.py
+++ Distributed/distributed_mnist.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import json
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 import tensorflow as tf
 import tensorflow_datasets as tfds

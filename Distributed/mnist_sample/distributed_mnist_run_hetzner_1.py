f08c43e3142c40a80956a71b52aca7764a8074e0
leo <leo.ertuna@gmail.com>
2019-09-25 15:45:50 +0300

    signle worker run scripts

---
+++ Distributed/mnist_sample/distributed_mnist_run_hetzner_1.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import json
+import os
+from distributed_mnist import main
+
+# Create cluster
+# This variable must be set on each worker with changing index
+os.environ["TF_CONFIG"] = json.dumps({
+    'cluster': {
+        'worker': ["178.63.21.5:2222"]
+    },
+    'task': {'type': 'worker', 'index': 0}
+})
+
+main()
+
+exit()

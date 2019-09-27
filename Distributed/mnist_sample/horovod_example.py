8c3ec30d91d3408048a8f3f9d16d503fec005284
leo <leo.ertuna@gmail.com>
2019-09-27 13:49:07 +0300

    Use arch 3

--- Distributed/mnist_sample/horovod_example.py
+++ Distributed/mnist_sample/horovod_example.py
@@ -52,15 +52,16 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                activation='relu',
-                input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dense(2048, activation='relu'))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
 # Horovod: adjust learning rate based on number of GPUs.
